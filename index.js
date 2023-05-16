57f8008437da3788c08399d571e4d9a59765b0b3
selahjs <sjemal99@gmail.com>
2023-05-16 11:05:51 +0300

    fixed loggin response

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express()
 const port = 3001
 
 app.use(cors());
+//we must use to parse our reqest body to json or req.body will be undefined
 app.use(express.json());
 
 app.get('/', (req, res)=>{
@@ -14,14 +15,13 @@ app.get('/', (req, res)=>{
 })
 
 app.post('/api/user/login', (req, res)=>{
-    console.log(req.body)
     const {username, password} = req.body
     if(username==='selahjemal@gmail.com' && password==='Pa$$w0rd'){
         res.status(200).json({username, token:'123abc'})
         return
     }
     //401 unauthorized
-    res.status(401).json({msg: 'Incorrect username or Password'})
+    res.status(401).json({error: 'Incorrect username or Password'})
 })
 app.listen(port, ()=>{
     console.log(`listning on port ${port}`)
